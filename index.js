552954b09e64f9857a62eecc91de8aa738742ce4
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-01-23 17:54:25 -0300

    update

--- index.js
+++ index.js
@@ -4,64 +4,80 @@ const copiar = document.getElementById("copy");
 const message = document.getElementById("texto");
 const campoResultado = document.getElementById("result-message");
 
-criptografar.addEventListener("click", function(e) {
-    e.preventDefault();
-    if (!message) {
-        alert("Por favor digite uma mensagem")
-    }
-    
-    const texto = message.value.toLowerCase();
-    
-   
+criptografar.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!message) {
+    alert("Por favor digite uma mensagem");
+  }
+
+  const texto = message.value.toLowerCase();
+
+  if (
+    texto.indexOf("ufat") !== -1 ||
+    texto.indexOf("imes") !== -1 ||
+    texto.indexOf("enter") !== -1 ||
+    texto.indexOf("ober") !== -1 ||
+    texto.indexOf("ai") !== -1
+  ) {
+    alert("Essa mensagem ja esta criptograda, tente descriptografar");
+  } else {
     let fraseCripto = "";
 
     for (let i = 0; i < texto.length; i++) {
-        if (texto[i] === "a") {
-            fraseCripto += "ai"
-        } else if (texto[i] === "e") {
-            fraseCripto += "enter"
-        } else if (texto[i] === "i") {
-            fraseCripto += "imes"
-        } else if (texto[i] === "o") {
-            fraseCripto += "ober"
-        } else if (texto[i] === "u") {
-            fraseCripto += "ufat"
-        } else {
-            fraseCripto += texto[i]
-        }
+      if (texto[i] === "a") {
+        fraseCripto += "ai";
+      } else if (texto[i] === "e") {
+        fraseCripto += "enter";
+      } else if (texto[i] === "i") {
+        fraseCripto += "imes";
+      } else if (texto[i] === "o") {
+        fraseCripto += "ober";
+      } else if (texto[i] === "u") {
+        fraseCripto += "ufat";
+      } else {
+        fraseCripto += texto[i];
+      }
     }
-    message.value = ""    
+    message.value = "";
     campoResultado.value = fraseCripto;
-})
+  }
+});
 
-copiar.addEventListener("click", function(e) {
-    e.preventDefault();
-    campoResultado.select();
-    document.execCommand("cut");
-    campoResultado.value = "";
-    alert("Messagem copiada com sucesso");
-    
-})
+copiar.addEventListener("click", function (e) {
+  e.preventDefault();
+  campoResultado.select();
+  document.execCommand("cut");
+  campoResultado.value = "";
+  alert("Messagem copiada com sucesso");
+});
 
-descriptografar.addEventListener("click", function(e) {
-    e.preventDefault();
-    if (!message) {
-        alert("Por favor digite uma mensagem")
+descriptografar.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!message) {
+    alert("Por favor digite uma mensagem");
+  }
+  let texto = message.value.toLowerCase();
+
+  console.log(texto.indexOf("enter"));
+
+  if (
+    texto.indexOf("ufat") !== 1 &
+    texto.indexOf("imes") !== 1 &
+    texto.indexOf("enter") !== 1 &
+    texto.indexOf("ober") !== 1 &
+    texto.indexOf("ai") !== 1
+  ) {
+    alert("Essa mensagem ja esta descriptografada, tente criptografar");
+  } else {
+    message.value = "";
+
+    for (let i = 0; i < texto.length; i++) {
+      texto = texto.replace("ai", "a");
+      texto = texto.replace("enter", "e");
+      texto = texto.replace("imes", "i");
+      texto = texto.replace("ober", "o");
+      texto = texto.replace("ufat", "u");
     }
-    let texto = message.value.toLowerCase();
-    
-    message.value = ""
-
-    for(let i = 0; i < texto.length; i++) {
-        texto = texto.replace("ai", "a")
-        texto = texto.replace("enter", "e")
-        texto = texto.replace("imes", "i")
-        texto = texto.replace("ober", "o")
-        texto = texto.replace("ufat", "u")
-    }
-   
-
     campoResultado.value = texto;
-    
-
-})
+  }
+});
